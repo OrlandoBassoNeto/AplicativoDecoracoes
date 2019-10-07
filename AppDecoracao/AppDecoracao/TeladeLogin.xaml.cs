86f0aa25c3aab81282d216c0bca53bba0c435461
android neon <androidneon@hotmail.com>
2019-10-07 17:32:08 -0300

    Adicionar arquivos de projeto.

---
+++ AppDecoracao/AppDecoracao/TeladeLogin.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppDecoracao
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TeladeLogin : ContentPage
+	{
+		public TeladeLogin ()
+		{
+			InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+		}
+
+        private void ButtonLogar_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(entryUsuario.Text) ||
+            string.IsNullOrEmpty(entrySenha.Text))
+            {
+                DisplayAlert("ERRO!", "°Não deixe os campos em brancos.", "OK");
+
+
+            }
+            else if (entryUsuario.Text == "Orlando" || entrySenha.Text == "123")
+            {
+                Navigation.PushAsync(new MainPage());
+            }
+
+            else
+            {
+                DisplayAlert("ERRO!", "°Senha ou Nome Incorretos", "OK");
+
+            }
+
+        }
+
+        private void ButtonLimpar_Clicked(object sender, EventArgs e)
+        {
+            entryUsuario.Text = "";
+            entrySenha.Text = "";
+
+        }
+    }
+}
